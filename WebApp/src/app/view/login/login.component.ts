ac8078d99e586228c6fcffc37da12613645bd589
Tamas Varadi <varadi.thomas@gmail.com>
2022-12-01 22:41:08 +0100

    login view sitebuild

--- WebApp/src/app/view/login/login.component.ts
+++ WebApp/src/app/view/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  passwordFormControl = new FormControl('', [Validators.required]);
 
   constructor() { }
 
